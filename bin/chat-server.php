35b9ec466e51cad25d352e185bee371ad78cc676
Yehor Konoval <ekonoval@gmail.com>
2015-02-10 11:07:55 +0200

    init

---
+++ bin/chat-server.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+use MyApp\Chat;
+use Ratchet\Server\IoServer;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+//$obj = new Chat();
+//$obj->test();exit;
+
+
+$server = IoServer::factory(
+    new Chat(),
+    8080
+);
+
+$server->run();
