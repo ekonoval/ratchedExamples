29c9dd47de90922c913dc7c029b6ecea15cd4f38
Yehor Konoval <ekonoval@gmail.com>
2015-02-10 11:35:01 +0200

    tes ws and js

--- bin/ws-server.php
+++ bin/ws-server.php
@@ -2,19 +2,20 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-use MyApp\Chat;
 use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\Chat;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-//$obj = new Chat();
-//$obj->test();exit;
-
-
 $server = IoServer::factory(
-    new Chat(),
-    8081,
-    'localhost'
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    8080
 );
 
 $server->run();
